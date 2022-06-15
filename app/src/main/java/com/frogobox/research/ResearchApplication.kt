516eb16d1760a8abd15a4ec4dbeb3c28dcb2b864
amirisback <faisalamircs@gmail.com>
2022-06-15 15:53:12 +0700

    setup hilt

---
+++ app/src/main/java/com/frogobox/research/ResearchApplication.kt
@@ -0,0 +1,11 @@
+package com.frogobox.research
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+// Created by KoinWorks (M. Faisal Amir) on 15/06/22.
+
+
+@HiltAndroidApp
+class ResearchApplication : Application() {
+}
